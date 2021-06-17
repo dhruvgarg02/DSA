198a3c4318558756c5092bba1039ea3a1d6ed8ea
Dhruv Garg <dhruvgargg02@gmail.com>
2021-06-18 02:04:48 +0530

    Added cycles method in matrix rotation

--- Arrays/rotate-matrix-90deg-anticlockwise.cpp
+++ Arrays/rotate-matrix-90deg-anticlockwise.cpp
@@ -2,12 +2,31 @@
 typedef long long int ll;
 using namespace std;
 
-void rotate(vector<vector<int>>& v, int n) {
+void transpose(vector<vector<int>>& v, int n) {
     for (int i = 0; i < n; i++)
         for (int j = i; j < n; j++)
             swap(v[i][j], v[j][i]);
 
     reverse(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++) {
+        cout<<endl;
+        for (int j = 0; j < n; j++)
+            cout<<v[i][j]<<" ";
+    }
+    return;
+}
+
+void cycles(vector<vector<int>>& v, int n) {
+    for (int i = 0; i < n/2; i++) {
+        for (int j = i; j < n-i-1; j++) {
+            int temp = v[i][j];
+            v[i][j] = v[j][n-i-1];
+            v[j][n-i-1] = v[n-i-1][n-j-1];
+            v[n-i-1][n-j-1] = v[n-j-1][i];
+            v[n-j-1][i] = temp;
+        }
+    }
 
     for (int i = 0; i < n; i++) {
         cout<<endl;
@@ -30,6 +49,9 @@ int main() {
         }
         v.push_back(temp);
     }
-    rotate(v, n);
+    //Two methods are implemented
+    // comment out one of the following methods
+    transpose(v, n);
+    cycles(v, n);
     return 0;
 }
