e3ebb1791388970038c7faca3862ec9de928de64
Dhruv Garg <dhruvgargg02@gmail.com>
2021-09-12 16:56:00 +0530

    print LCS

--- Dynamic Programming/LCS/print_LCS.cpp
+++ Dynamic Programming/LCS/print_LCS.cpp
@@ -29,7 +29,7 @@ int main() {
     }
     vector<int> res;
     int i = n, j = m;
-    while(i > 0 && j > 0) {
+    while(i > 0 && j > 0) { 
         if (a[i-1] == b[j-1]) {
             res.push_back(a[i-1]);
             i--; j--;
